c1fcfaa9f64904b0026cf89de9295de28f53f209
lol <lol@DESKTOP-EDO0K66>
2023-06-01 02:54:08 -0700

    more

---
+++ sheep-injector/MainForm.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace sheep_injector
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnInject = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tmrProcesses = new System.Windows.Forms.Timer(this.components);
+            this.listProcesses = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listDLLs = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 464);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnInject
+            // 
+            this.btnInject.Location = new System.Drawing.Point(93, 464);
+            this.btnInject.Name = "btnInject";
+            this.btnInject.Size = new System.Drawing.Size(75, 23);
+            this.btnInject.TabIndex = 2;
+            this.btnInject.Text = "Inject";
+            this.btnInject.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Processes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "DLLs";
+            // 
+            // tmrProcesses
+            // 
+            this.tmrProcesses.Interval = 500;
+            this.tmrProcesses.Tick += new System.EventHandler(this.tmrProcesses_Tick);
+            // 
+            // listProcesses
+            // 
+            this.listProcesses.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listProcesses.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listProcesses.FullRowSelect = true;
+            this.listProcesses.HideSelection = false;
+            this.listProcesses.Location = new System.Drawing.Point(242, 25);
+            this.listProcesses.MultiSelect = false;
+            this.listProcesses.Name = "listProcesses";
+            this.listProcesses.Size = new System.Drawing.Size(242, 431);
+            this.listProcesses.TabIndex = 7;
+            this.listProcesses.UseCompatibleStateImageBehavior = false;
+            this.listProcesses.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 145;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "PID";
+            // 
+            // listDLLs
+            // 
+            this.listDLLs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listDLLs.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listDLLs.FullRowSelect = true;
+            this.listDLLs.HideSelection = false;
+            this.listDLLs.Location = new System.Drawing.Point(15, 25);
+            this.listDLLs.MultiSelect = false;
+            this.listDLLs.Name = "listDLLs";
+            this.listDLLs.Size = new System.Drawing.Size(221, 431);
+            this.listDLLs.TabIndex = 8;
+            this.listDLLs.UseCompatibleStateImageBehavior = false;
+            this.listDLLs.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Name";
+            this.columnHeader3.Width = 145;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Arch";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "LoadLibraryA"});
+            this.comboBox1.Location = new System.Drawing.Point(174, 466);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(310, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.Text = "LoadLibraryA";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 494);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listDLLs);
+            this.Controls.Add(this.listProcesses);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnInject);
+            this.Controls.Add(this.btnOpen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sheep Injector";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnInject;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer tmrProcesses;
+        private System.Windows.Forms.ListView listProcesses;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ListView listDLLs;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ sheep-injector/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sheep_injector
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private static string dllPath = string.Empty;
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    dllPath = ofd.FileName;
+
+                    listDLLs.Items.Add(new ListViewItem(new string[] { dllPath, "bleh :P" }));
+                }
+            }
+        }
+
+        private void tmrProcesses_Tick(object sender, EventArgs e)
+        {
+            // todo: make it update when a new process is started
+            Process[] procs = Process.GetProcesses();
+
+            foreach (ListViewItem item in listProcesses.Items)
+            {
+                if (procs.Where(p => p.Id == ((Process)item.Tag).Id).Count() == 0) 
+                {
+                    item.Remove();
+                }
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Process[] procs = Process.GetProcesses();
+            foreach (Process proc in procs) 
+            {
+                ListViewItem item = new ListViewItem(new string[] { proc.ProcessName, proc.Id.ToString() });
+                item.Tag = proc;
+                listProcesses.Items.Add(item);
+            }
+
+            tmrProcesses.Start();
+        }
+    }
+}

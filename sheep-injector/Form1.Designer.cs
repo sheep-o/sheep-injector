b40dd3a98325d7f3b8c528ea68ebcb73a72c001c
lol <lol@DESKTOP-EDO0K66>
2023-06-01 01:47:16 -0700

    Add project files.

---
+++ sheep-injector/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace sheep_injector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lblDLLPath = new System.Windows.Forms.Label();
+            this.btnInject = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 42);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lblDLLPath
+            // 
+            this.lblDLLPath.AutoSize = true;
+            this.lblDLLPath.Location = new System.Drawing.Point(13, 13);
+            this.lblDLLPath.Name = "lblDLLPath";
+            this.lblDLLPath.Size = new System.Drawing.Size(52, 13);
+            this.lblDLLPath.TabIndex = 1;
+            this.lblDLLPath.Text = "DLL Path";
+            // 
+            // btnInject
+            // 
+            this.btnInject.Location = new System.Drawing.Point(93, 42);
+            this.btnInject.Name = "btnInject";
+            this.btnInject.Size = new System.Drawing.Size(75, 23);
+            this.btnInject.TabIndex = 2;
+            this.btnInject.Text = "Inject";
+            this.btnInject.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 75);
+            this.Controls.Add(this.btnInject);
+            this.Controls.Add(this.lblDLLPath);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sheep Injector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label lblDLLPath;
+        private System.Windows.Forms.Button btnInject;
+    }
+}
+
